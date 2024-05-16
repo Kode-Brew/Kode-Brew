ef691156c1be428f1006b6d851a9adc8bc25fa10
Giovanna Perri <giovannaperri@outlook.com>
2024-05-16 20:23:58 -0300

    crud in progress

---
+++ app/controllers/project_members_controller.rb
@@ -0,0 +1,2 @@
+class ProjectMembersController < ApplicationController
+end
---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+
+  def index
+  end
+
+  def showes
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
