fc6d08174092cfd740004b8aeec57ec199bcba50
Beatriz Aranha <be.aranha@gmail.com>
2024-05-20 14:58:03 -0300

    fix: creates validation to Sprint model

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -2,4 +2,6 @@ class Sprint < ApplicationRecord
   belongs_to :project
   has_many :sprint_lectures # not dependent: :destroy
   has_many :tasks, dependent: :destroy
+
+  validates :objective, :date_start, :date_end, presence: true
 end
