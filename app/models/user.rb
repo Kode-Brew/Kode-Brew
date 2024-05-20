fe30dad2aeaee0f5d60ed1acd65b96b080bd6262
anthony vieira <thony16@gmail.com>
2024-05-20 16:08:40 -0300

    feat: relations users/lectures/tickets/sprint_lectures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :project_members
+  has_many :sprint_lectures
+  has_many :ticket
+  has_many :lectures, trough: :sprint_lectures
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
