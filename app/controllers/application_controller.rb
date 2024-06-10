97a7f595a60977e38e34facdd0eee60fd14f3cab
Caio Brasetti <caiobrasetti@hotmail.com>
2024-06-10 15:49:12 -0300

    feat: Giving permission and adding the cloudinary link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role points])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name role points])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role points photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name role points photo])
   end
 end
