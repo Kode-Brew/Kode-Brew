1eb673bcb86ea30d0f1b8aeacb126a0090aa9be9
Beatriz Aranha <be.aranha@gmail.com>
2024-06-17 15:09:28 -0300

    feat: makes methods to sprint dates validations

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -4,7 +4,23 @@ class Sprint < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :lectures, through: :sprint_lectures
 
+  before_validation :set_date_end
   validates :objective, :date_start, :date_end, presence: true
 
+  validate :date_start_after_previous_sprint
 
+  private
+
+  def set_date_end
+    self.date_end = date_start + 6.days if date_start
+  end
+
+  def date_start_after_previous_sprint
+    if date_start && project
+      previous_sprint = project.sprints.where("date_end < ?", date_start).order(date_end: :desc).first
+      if previous_sprint && date_start <= previous_sprint.date_end
+        errors.add(:date_start, "A data de início da nova Sprint deve ser posterior a #{previous_sprint.date_end.strftime("%d/%m/%Y")}.")
+      end
+    end
+  end
 end
