425987ad9b903afdf4252b2f45bba3a21550c00f
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-06-18 12:39:14 -0300

    feat: adjustments to the controller and form for the new columns inserted

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,10 @@
 class Ticket < ApplicationRecord
+  enum category: {
+    Backend: 0,
+    FrontEnd: 1,
+    FullStack: 2,
+    Deploy: 3
+  }
   # Associations
   belongs_to :task
   belongs_to :user
