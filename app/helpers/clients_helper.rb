7b21d8816aec5aaf5d4b14bba6ebd614358b1a19
Beatriz Aranha <be.aranha@gmail.com>
2024-07-10 13:58:23 -0300

    feat: completes backend of Clients index

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,5 +1,13 @@
 module ClientsHelper
-  def project_count_for_status(client, status)
-    client.projects.where(status: status).count
-   end
+  def ongoing_projects_count(client)
+    client.projects.where(status: [:Pendente, :Iniciado], is_active?: true).count
+  end
+
+  def finished_projects_count(client)
+    client.projects.where(status: :Finalizado).count
+  end
+
+  def inactive_projects_count(client)
+    client.projects.where(is_active?: false).count
+  end
 end
