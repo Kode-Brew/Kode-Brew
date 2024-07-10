806bc9caaff066de2c285a3a3a9103793c1bab79
Beatriz Aranha <be.aranha@gmail.com>
2024-07-10 13:49:11 -0300

    feat: changes Project models - is_active is set to true by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_203325) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_164202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,7 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_01_203325) do
     t.string "name"
     t.string "category"
     t.string "description"
-    t.boolean "is_active?"
+    t.boolean "is_active?", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "active_sprint", default: 1
