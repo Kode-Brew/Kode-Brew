94b7d61ab442c64bb324efcf2c3078c015c8fc4c
anthony vieira <thony16@gmail.com>
2024-07-11 21:53:43 -0300

    Feat: Lectures forms new and edit

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,4 +1,6 @@
 class Lecture < ApplicationRecord
   belongs_to :user, optional: true
   validates :title, :source, :video, :slide, presence: true
+
+  TAGS = %w[Front-end Back-end Full-stack Javascript CSS Deploy].sort
 end
