f92a5dad5254cc504739ed9185702f80952479ab
Caio Brasetti <caiobrasetti@hotmail.com>
2024-07-22 14:15:55 -0300

    feat: Adding the logic of the 'pg_search' gem to the lectures model and controller

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,6 +1,14 @@
 class Lecture < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :user, optional: true
   validates :title, :video, presence: true
 
   TAGS = %w[Front-end Back-end Full-stack Javascript CSS Deploy].sort
+
+  pg_search_scope :search_by_title_and_tag,
+                  against: %i[title tag],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
